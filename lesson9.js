04fd45bbc7eafcadbc1a8d5de7be16c67b79603e
Medina <medinasulaymanova16@gmail.com>
2022-02-23 19:17:35 +0300

    commit

---
+++ lesson9.js
@@ -0,0 +1,71 @@
+let countsm = 1,
+    countbg = 10,
+    win = getRandomNum(countsm, countbg),
+    guessesLeft = 3;
+
+const game = document.querySelector("#game"),
+    minNum = document.querySelector(".min-num"),
+    maxNum = document.querySelector(".max-num"),
+    guessBtn = document.querySelector("#guess-btn"),
+    guessInput = document.querySelector("#guess-input"),
+    message = document.querySelector(".message");
+
+minNum.textContent = countsm;
+maxNum.textContent = countbg;
+
+guessBtn.addEventListener("click", function () {
+    let guess = parseInt(guessInput.value);
+
+    if (isNaN(guess) || guess < countsm || guess > countbg) {
+        setMessage(`Нужно ввести число от ${countsm} до ${countbg}`, "red");
+    }
+
+    if (guess === win) {
+        gameOver(true, `Поздравляю! Вы угадали число ${win}`);
+    } else {
+        guessesLeft -= 1;
+
+        if (guessesLeft === 0) {
+            gameOver(false, `Вы проиграли! Правильный ответ ${win}`);
+        } else {
+            guessInput.style.border = "1px solid red";
+            guessInput.value = "";
+            setMessage(`${guess} - Неверно! У вас осталось ${guessesLeft} попыток`, "red");
+        }
+
+    }
+});
+
+
+game.addEventListener("mousedown", function (e) {
+    if (e.target.className === "play-again") {
+        window.location.reload();
+    }
+});
+
+function gameOver(won, msg) {
+    let color;
+    if (won === true) {
+        (color = "green")
+    } else {
+        (color = "red");
+    }
+
+    guessInput.disabled = true;
+    guessInput.style.border = `1px solid ${color}`;
+    message.style.color = color;
+    setMessage(msg);
+
+
+    guessBtn.textContent = "Начать заново";
+    guessBtn.className += "play-again";
+}
+
+function setMessage(msg, color) {
+    message.textContent = msg;
+    message.style.color = color;
+}
+
+function getRandomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
